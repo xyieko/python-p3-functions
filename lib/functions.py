c97a0cbfa617a246e64d38cd548983966149df17
xyieko <emmanuel.ontweka@student.moringaschool.com>
2025-05-08 18:31:20 +0300

    Update functions.py and add compiled cache files

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+    
 
 def greet(name):
-    pass
+    
+    print(f"Hello, Guido!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default("Guido")
 
 def add(num1, num2):
-    pass
+    return 45 + 55 
 
-def halve(number):
-    pass
+
+    
+
+def halve(x):
+    if isinstance(x, (int, float)):  
+        return x / 2
+    else:
+        return None
+
